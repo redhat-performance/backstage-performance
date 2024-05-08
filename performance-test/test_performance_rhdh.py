5af740b362e7cc28489595fe792975c13a2448cb
Rajaditya Chauhan <rajchauh@redhat.com>
2024-05-08 17:58:33 +0530

    pulling up performance metrics while the page is in loading state. Additionally, commenting rest other tests for now.

--- performance-test/test_performance_rhdh.py
+++ performance-test/test_performance_rhdh.py
@@ -51,18 +51,27 @@ def test_guest_enter(page: Page, access):
 
     # Get CPU times before opening the browser
     cpu_times_before = p.cpu_times()
-    print("before:", cpu_times_before)
 
     expected_title = "Welcome back! | Red Hat Developer Hub"
 
     # Wait for the page to load and assert the title
     for i in range(100):
+
         page.wait_for_selector('//h1[contains(text(),"Welcome back!")]')
+
+        print(
+            page.evaluate("window.performance.getEntriesByType('paint')")
+        )  # info about painting the page (rendering)
+        print(
+            page.evaluate("performance.getEntriesByType('navigation')")
+        )  # info about DOM complete duration etc
         assert page.title() == expected_title
         page.reload()
 
     # Get CPU times after the page has loaded
     cpu_times_after = p.cpu_times()
+
+    print("before:", cpu_times_before)
     print("after:", cpu_times_after)
 
     # Calculate CPU time differences
@@ -74,14 +83,6 @@ def test_guest_enter(page: Page, access):
     children_system_time_diff = (
         cpu_times_after.children_system - cpu_times_before.children_system
     )
-
-    # Print performance information
-    print(
-        page.evaluate("window.performance.getEntriesByType('paint')")
-    )  # info about painting the page (rendering)
-    print(
-        page.evaluate("performance.getEntriesByType('navigation')")
-    )  # info about DOM complete duration etc
 
     # Print CPU time differences
     print("User CPU time during page load:", user_time_diff)
@@ -95,86 +96,86 @@ def test_guest_enter(page: Page, access):
 # pass
 
 
-def test_search_bar(page: Page, access):
+# def test_search_bar(page: Page, access):
 
-    searchBar = page.wait_for_selector(
-        "#search-bar-text-field"
-    )  # here '#' is used to grep id tag elements
-    searchBar.type("demo")  # Type demo in big search bar
-    searchBar.press("Enter")  # Press enter after typing demo
+#     searchBar = page.wait_for_selector(
+#         "#search-bar-text-field"
+#     )  # here '#' is used to grep id tag elements
+#     searchBar.type("demo")  # Type demo in big search bar
+#     searchBar.press("Enter")  # Press enter after typing demo
 
-    markElement = page.wait_for_selector(
-        '//mark[contains(text(), "demo")]'
-    )  # We are catching the element with tag and text since class is dynamic and changes with every reload
-    markElement.click()
+#     markElement = page.wait_for_selector(
+#         '//mark[contains(text(), "demo")]'
+#     )  # We are catching the element with tag and text since class is dynamic and changes with every reload
+#     markElement.click()
 
-    page.wait_for_timeout(5000)  # 5 seconds delay (5000 milliseconds)
-    assert page.title() == "demo | Overview | Red Hat Developer Hub"
-
-
-def test_learning_path(page: Page, access):
-
-    learningPathButton = page.wait_for_selector(
-        '//span[contains(text(), "Learning Paths")]'
-    )
-    learningPathButton.click()
-
-    availableOptions = [
-        "Building Operators on OpenShift",
-        "Configure a Jupyter notebook to use GPUs for AI/ML modeling",
-        "Deploy a Spring application on OpenShift",
-        "Deploy and manage your first API",
-    ]  # Many available picked few of them
-
-    for index, option in enumerate(availableOptions, start=1):
-        print(f"{index}. {option}")
-
-    desiredChoice = ""  # This is what user wants to press on Learning Paths
-    while True:
-        choice = input("Enter the number corresponding to your choice:")
-        try:
-            choice_index = int(choice)
-            if 1 <= choice_index <= len(availableOptions):
-                desiredChoice = availableOptions[choice_index - 1]  # Store in variable
-                break
-            else:
-                print("Invalid choice. Please enter a valid number.")
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-
-    learningSelect = page.wait_for_selector(
-        f'//span[contains(text(), "{desiredChoice}")]'
-    )
-    learningSelect.click()
-    page.wait_for_timeout(5000)  # 5 seconds delay (5000 milliseconds)
+#     page.wait_for_timeout(5000)  # 5 seconds delay (5000 milliseconds)
+#     assert page.title() == "demo | Overview | Red Hat Developer Hub"
 
 
-def test_catalog(page: Page, access):
+# def test_learning_path(page: Page, access):
 
-    catalog = page.wait_for_selector('//span[contains(text(), "Catalog")]')
-    catalog.click()
+#     learningPathButton = page.wait_for_selector(
+#         '//span[contains(text(), "Learning Paths")]'
+#     )
+#     learningPathButton.click()
 
-    createButton = page.wait_for_selector('//span[contains(text(), "Create")]')
-    createButton.click()
+#     availableOptions = [
+#         "Building Operators on OpenShift",
+#         "Configure a Jupyter notebook to use GPUs for AI/ML modeling",
+#         "Deploy a Spring application on OpenShift",
+#         "Deploy and manage your first API",
+#     ]  # Many available picked few of them
 
-    registerExistingComponent = page.wait_for_selector(
-        '//span[contains(text(), "Register Existing Component")]'
-    )
-    registerExistingComponent.click()
+#     for index, option in enumerate(availableOptions, start=1):
+#         print(f"{index}. {option}")
 
-    url = page.wait_for_selector("#url")
-    url.type("https://github.com/backstage/backstage/blob/master/catalog-info.yaml")
+#     desiredChoice = ""  # This is what user wants to press on Learning Paths
+#     while True:
+#         choice = input("Enter the number corresponding to your choice:")
+#         try:
+#             choice_index = int(choice)
+#             if 1 <= choice_index <= len(availableOptions):
+#                 desiredChoice = availableOptions[choice_index - 1]  # Store in variable
+#                 break
+#             else:
+#                 print("Invalid choice. Please enter a valid number.")
+#         except ValueError:
+#             print("Invalid input. Please enter a number.")
 
-    analyzeButton = page.wait_for_selector('//span[contains(text(), "Analyze")]')
-    analyzeButton.click()
+#     learningSelect = page.wait_for_selector(
+#         f'//span[contains(text(), "{desiredChoice}")]'
+#     )
+#     learningSelect.click()
+#     page.wait_for_timeout(5000)  # 5 seconds delay (5000 milliseconds)
 
-    page.wait_for_timeout(2000)  # 2 seconds for processing
-    importButton = page.wait_for_selector('//span[contains(text(),"Import")]')
-    importButton.click()
 
-    viewComponent = page.wait_for_selector('//span[contains(text(),"View Component")]')
-    viewComponent.click()
+# def test_catalog(page: Page, access):
 
-    page.wait_for_timeout(2000)  # 2 seconds for processing
-    assert page.title() == "backstage | Overview | Red Hat Developer Hub"
-    page.wait_for_timeout(5000)
+#     catalog = page.wait_for_selector('//span[contains(text(), "Catalog")]')
+#     catalog.click()
+
+#     createButton = page.wait_for_selector('//span[contains(text(), "Create")]')
+#     createButton.click()
+
+#     registerExistingComponent = page.wait_for_selector(
+#         '//span[contains(text(), "Register Existing Component")]'
+#     )
+#     registerExistingComponent.click()
+
+#     url = page.wait_for_selector("#url")
+#     url.type("https://github.com/backstage/backstage/blob/master/catalog-info.yaml")
+
+#     analyzeButton = page.wait_for_selector('//span[contains(text(), "Analyze")]')
+#     analyzeButton.click()
+
+#     page.wait_for_timeout(2000)  # 2 seconds for processing
+#     importButton = page.wait_for_selector('//span[contains(text(),"Import")]')
+#     importButton.click()
+
+#     viewComponent = page.wait_for_selector('//span[contains(text(),"View Component")]')
+#     viewComponent.click()
+
+#     page.wait_for_timeout(2000)  # 2 seconds for processing
+#     assert page.title() == "backstage | Overview | Red Hat Developer Hub"
+#     page.wait_for_timeout(5000)
