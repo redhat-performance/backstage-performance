f55d78718dc4c653ecff2b1048b5f297140c9885
Rajaditya Chauhan <rajchauh@redhat.com>
2024-05-22 11:02:20 +0530

    modifying code base to track memory, cpu per reload. Adding hostname and current time of reload in csv file along with some other metrics.

--- performance-test/rhdh_ui_load_perf.py
+++ performance-test/rhdh_ui_load_perf.py
@@ -2,41 +2,45 @@
 
 from playwright.sync_api import sync_playwright
 import time
-import os
-import psutil
-import csv
-from utils.monitor import (
-    MemoryMonitor,
-)  # this is a custom utility for monitoring memory
-from rich.progress import Progress
-from rich.console import Console
+import os # to capture pid of current process
+import psutil # to capture cpu and memory metrics
+import csv # to capture ,metrics into a csv file
+import socket # to capture hostname
+from utils.monitor import MemoryMonitor  # custom utility for monitoring memory
+from rich.progress import Progress # to display progress bar for relaods
+from rich.console import Console # to print in a nice way
+from rich.table import Table
+import pandas as pd # for printing csv file in human readable format
 
 monitor = None
 console = Console()
 
-
 def setup_monitoring():
     global monitor
     console.print("\n[bold green]Setting up monitoring...[/bold green]")
-    # Start monitoring thread
     monitor = MemoryMonitor()
     monitor.start()
-
 
 def teardown_monitoring():
     global monitor
     monitor.stop()
     monitor.join()
 
-    # Calculate average memory usage
+def get_memory_info():
+    global monitor
     avg_rss = sum(monitor.rss_usage) / len(monitor.rss_usage) / (1024 * 1024)
     avg_vms = sum(monitor.vms_usage) / len(monitor.vms_usage) / (1024 * 1024)
     avg_shared = sum(monitor.shared_usage) / len(monitor.shared_usage) / (1024 * 1024)
+    return avg_rss, avg_vms, avg_shared
 
-    console.print(
-        f"[bold yellow]Average Memory Usage (RSS, VMS, Shared):[/bold yellow] {avg_rss:.2f} MB, {avg_vms:.2f} MB, {avg_shared:.2f} MB"
-    )
-
+def print_csv_as_table(file_path):
+    df = pd.read_csv(file_path)
+    table = Table(show_header=True, header_style="bold magenta")
+    for column in df.columns:
+        table.add_column(column)
+    for _, row in df.iterrows():
+        table.add_row(*map(str, row.values))
+    console.print(table)
 
 def main():
     rhdh_endpoint = os.environ.get("endpoint")
@@ -44,18 +48,19 @@ def main():
     if not rhdh_endpoint:
         raise ValueError("RHDH_ENDPOINT environment variable is not set.")
 
+    hostname = socket.gethostname()
+    
     with sync_playwright() as p:
-
-        # csv report init
-        csv_file_obj = open("test.csv", "w", newline="")
+        csv_file_path = "test.csv"
+        csv_file_obj = open(csv_file_path, "w", newline="")
         writer = csv.writer(csv_file_obj)
         writer.writerow(
             [
-                "first-paint_start_time",
-                "first-contentful-paint_start_time",
-                "domContentLoadedEventStart",
-                "domContentLoadedEventEnd",
-                "domComplete",
+                "current_time", "hostname",
+                "first-paint_start_time", "first-contentful-paint_start_time",
+                "domContentLoadedEventStart", "domContentLoadedEventEnd", "domComplete",
+                "cputimes_user", "cputimes_system", "cputimes_iowait",
+                "rss_memory", "vms_memory", "shared_memory"
             ]
         )
 
@@ -67,95 +72,49 @@ def main():
 
         page.goto(rhdh_endpoint)
 
-        # Click the 'Enter' button after the page has loaded
         guest_enter_button = page.wait_for_selector(
             '//span[@class="MuiButton-label-222" and text()="Enter"]'
         )
         guest_enter_button.click()
 
-        # Get the current process ID
         current_pid = os.getpid()
-
-        # Create a psutil.Process object for the current process
         process = psutil.Process(pid=current_pid)
-
-        # Get CPU times and IO counters before opening the browser
-        cpu_times_before = process.cpu_times()
-
         expected_title = "Welcome back! | Red Hat Developer Hub"
 
         with Progress() as progress:
             reload_task = progress.add_task("[cyan]Reloading...", total=customReload)
 
-            # Wait for the page to load and assert the title
             for i in range(customReload):
                 page.wait_for_selector('//h1[contains(text(),"Welcome back!")]')
+                paint_info = page.evaluate("window.performance.getEntriesByType('paint')")
+                navigation_info = page.evaluate("performance.getEntriesByType('navigation')")
 
-                # info about painting the page (rendering)
-                paint_info = page.evaluate(
-                    "\nwindow.performance.getEntriesByType('paint')"
-                )
-                # console.print(paint_info)
+                cpu_times = process.cpu_times()
+                avg_rss, avg_vms, avg_shared = get_memory_info()
 
-                # info about DOM complete duration etc
-                navigation_info = page.evaluate(
-                    "performance.getEntriesByType('navigation')"
-                )
-                # console.print(navigation_info)
+                current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
                 writer.writerow(
                     [
-                        paint_info[0]["startTime"],
-                        paint_info[1]["startTime"],
+                        current_time, hostname,
+                        paint_info[0]["startTime"], paint_info[1]["startTime"],
                         navigation_info[0]["domContentLoadedEventStart"],
                         navigation_info[0]["domContentLoadedEventEnd"],
                         navigation_info[0]["domComplete"],
+                        cpu_times.user, cpu_times.system, cpu_times.iowait,
+                        avg_rss, avg_vms, avg_shared
                     ]
                 )
                 assert page.title() == expected_title
                 page.reload()
-
                 progress.update(reload_task, advance=1)
 
-        # Get CPU times and IO counters after the page has loaded
-        cpu_times_after = process.cpu_times()
-
-        console.print("[bold blue]CPU times before:[/bold blue]", cpu_times_before)
-        console.print("[bold blue]CPU times after:[/bold blue]", cpu_times_after)
-
-        # Calculate CPU time differences
-        user_time_diff = cpu_times_after.user - cpu_times_before.user
-        system_time_diff = cpu_times_after.system - cpu_times_before.system
-        children_user_time_diff = (
-            cpu_times_after.children_user - cpu_times_before.children_user
-        )
-        children_system_time_diff = (
-            cpu_times_after.children_system - cpu_times_before.children_system
-        )
-
-        # Print CPU time differences
-        console.print(
-            "[bold green]User CPU time during page load:[/bold green]", user_time_diff
-        )
-        console.print(
-            "[bold green]System CPU time during page load:[/bold green]",
-            system_time_diff,
-        )
-        console.print(
-            "[bold green]Children User CPU time during page load:[/bold green]",
-            children_user_time_diff,
-        )
-        console.print(
-            "[bold green]Children System CPU time during page load:[/bold green]",
-            children_system_time_diff,
-        )
-
-        # close csv report file object
         csv_file_obj.close()
-
         teardown_monitoring()
         browser.close()
 
+        # Print CSV in a readable table format
+        print_csv_as_table(csv_file_path)
 
 if __name__ == "__main__":
     main()
