242f9051b886344bc8934133220c69e7188166d3
Pavel Macík <pavel.macik@gmail.com>
2024-12-18 13:38:18 +0100

    feat(WATCH_EXT_CONF): Introduce WATCH_EXT_CONF parameter variable for RHDH operator (#141)
    Signed-off-by: Pavel Macík <pavel.macik@gmail.com>

--- ci-scripts/dev-sandbox/collect-results.sh
+++ ci-scripts/dev-sandbox/collect-results.sh
@@ -13,6 +13,8 @@ export TMP_DIR
 TMP_DIR=$(readlink -m "${TMP_DIR:-"$rootdir/.tmp"}")
 mkdir -p "${TMP_DIR}"
 
+cli="oc"
+
 out=$ARTIFACT_DIR/dev-sandbox/summary.csv
 rm -rvf "$out"
 while read -r baseline_csv; do
@@ -46,3 +48,68 @@ while read -r baseline_counts_csv; do
         done <<<"$metrics"
     done <"$baseline_counts_csv"
 done <<<"$(find "${ARTIFACT_DIR}/dev-sandbox/" -name '*baseline-counts-post.csv')"
+
+echo "$(date --utc -Ins) Setting up tool to collect monitoring data"
+
+PYTHON_VENV_DIR="$rootdir/.venv"
+python3 -m venv "$PYTHON_VENV_DIR"
+set +u
+# shellcheck disable=SC1090,SC1091
+source "$PYTHON_VENV_DIR/bin/activate"
+set -u
+python3 -m pip install --quiet -U pip
+python3 -m pip install --quiet -e "git+https://github.com/redhat-performance/opl.git#egg=opl-rhcloud-perf-team-core&subdirectory=core"
+set +u
+deactivate
+set -u
+
+echo "$(date --utc -Ins) Collecting monitoring data"
+set +u
+# shellcheck disable=SC1090,SC1091
+source "$PYTHON_VENV_DIR/bin/activate"
+set -u
+
+monitoring_collection_data="${ARTIFACT_DIR}/benchmark.json"
+monitoring_collection_log=$ARTIFACT_DIR/monitoring-collection.log
+monitoring_collection_dir=$ARTIFACT_DIR/monitoring-collection-raw-data-dir
+mkdir -p "$monitoring_collection_dir"
+
+mstart=$(date --utc --date "$(cat "${ARTIFACT_DIR}/benchmark-before")" --iso-8601=seconds)
+mend=$(date --utc --date "$(cat "${ARTIFACT_DIR}/benchmark-after")" --iso-8601=seconds)
+mhost=$(kubectl -n openshift-monitoring get route -l app.kubernetes.io/name=thanos-query -o json | jq --raw-output '.items[0].spec.host')
+status_data.py \
+    --status-data-file "$monitoring_collection_data" \
+    --set \
+    name="RHDH on Developer Sandbox Benchmark" \
+    -d &>"$monitoring_collection_log"
+status_data.py \
+    --status-data-file "$monitoring_collection_data" \
+    --additional "$SCRIPT_DIR/metrics-config.yaml" \
+    --monitoring-start "$mstart" \
+    --monitoring-end "$mend" \
+    --monitoring-raw-data-dir "$monitoring_collection_dir" \
+    --prometheus-host "https://$mhost" \
+    --prometheus-port 443 \
+    --prometheus-token "$($cli whoami -t)" \
+    -d &>>"$monitoring_collection_log"
+
+while read -r metric_csv; do
+    {
+        tmp_csv="$metric_csv.tmp"
+        mv -f "$metric_csv" "$tmp_csv"
+        echo "Processing $metric_csv"
+        while read -r line; do
+            IFS=',' read -ra tokens <<<"$line"
+            timestamp="${tokens[0]}"
+            value="${tokens[1]}"
+            if [[ $line =~ ^timestamp ]]; then
+                echo "$line"
+            else
+                echo "$(date -d "@$timestamp" --utc -Ins),$value"
+            fi
+        done <"$tmp_csv" >>"$metric_csv"
+        rm -f "$tmp_csv"
+    } &
+done <<<"$(find "$monitoring_collection_dir" -name '*.csv')"
+
+wait
