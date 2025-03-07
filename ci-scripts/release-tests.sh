c2631076c6081175afb964a23b367007ad02afb5
Jan Hutar <jhutar@redhat.com>
2025-03-07 09:06:40 +0100

    fix: Resolve syntax error and few more issues found by shellcheck

--- ci-scripts/release-tests.sh
+++ ci-scripts/release-tests.sh
@@ -69,8 +69,11 @@ export RHDH_HELM_REPO='$RHDH_HELM_REPO'
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer $GITHUB_TOKEN" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        "https://api.github.com/repos/redhat-performance/backstage-performance/issues/$pr_number/comments "
+        "https://api.github.com/repos/redhat-performance/backstage-performance/issues/$pr_number/comments" \
         -d '{"body":"/test mvp-scalability"}'
+    )"
+    comment_url=$( echo "$curl_comment_out" | jq -rc '.html_url' )
+    echo "$(date -Ins --utc) INFO Triggered build by ${comment_url}"
 }
 
 function _test() {
