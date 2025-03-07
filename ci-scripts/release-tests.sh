fc97171acfbbcce33bcf2f0e794e81d1f2746aff
Jan Hutař <jhutar@redhat.com>
2025-03-07 09:24:53 +0100

    Update script creating test PRs (#157)
    * feat(RHIDP-5931): Update script for 1.5 testing
    
    * feat: Open a PR when branch gets pushed
    
    * fix: Correct command to trigger the test
    
    * feat: Base old commits on branch
    
    * fix: Remove this placeholder that was meant to be either OLD or NEW
    
    * test: We need more entities here to hit 1 GiB of DB storage now when we do not import that many users
    
    * fix: Base branch should not always be main
    
    Co-authored-by: Pavel Macík <pavel.macik@gmail.com>
    
    * fix: Use mvp-scalability and enable RBAC
    
    Co-authored-by: Pavel Macík <pavel.macik@gmail.com>
    
    * fix: Suggest correct test name
    
    * feat: Add trigger comment as part of the script
    
    Co-authored-by: Pavel Macík <pavel.macik@gmail.com>
    
    * fix: Resolve syntax error and few more issues found by shellcheck
    
    ---------
    
    Co-authored-by: Pavel Macík <pavel.macik@gmail.com>

--- ci-scripts/release-tests.sh
+++ ci-scripts/release-tests.sh
@@ -7,12 +7,16 @@ function configure_run() {
         echo "$(date -Ins --utc) FATAL Can not reach 'test.env' file. Are you in backstage-performance directory?"
         exit 1
     fi
+    if [ -z "$GITHUB_TOKEN" ]; then
+        echo "$(date -Ins --utc) FATAL Please export GITHUB_TOKEN. It is needed to create PRs."
+        exit 1
+    fi
 
     ticket="$1"
     branch="$2"
     testname="$3"
 
-    git checkout main
+    git checkout "$SOURCE_BRANCH"
     git checkout -b "$branch"
 
     echo "
@@ -26,6 +30,7 @@ export SCALE_DB_STORAGES='$SCALE_DB_STORAGES'
 export SCALE_MEMORY_REQUESTS_LIMITS=:
 export SCALE_REPLICAS=1
 export SCALE_WORKERS=20
+export ENABLE_RBAC=true
 export SCENARIO=mvp
 export USE_PR_BRANCH=true
 export WAIT_FOR_SEARCH_INDEX=false
@@ -36,8 +41,39 @@ export RHDH_HELM_REPO='$RHDH_HELM_REPO'
 
     git commit -am "chore($ticket): $testname on $branch"
     git push -u origin "$branch"
-    echo "$(date -Ins --utc) INFO Create PR on https://github.com/redhat-performance/backstage-performance/pull/new/${branch} and comment '/test mpc' there"
-    git checkout main
+    echo "$(date -Ins --utc) INFO Created and pushed branch ${branch}"
+    git checkout "$SOURCE_BRANCH"
+
+    curl_data='{
+        "title": "chore('"$ticket"'): '"$branch"'",
+        "body": "**'"$testname"'**: '"$VERSION_OLD"' vs. '"$VERSION_NEW"' testing. This is to get perf&scale data for `'"$branch"'`",
+        "head": "'"$branch"'",
+        "base": "'"$SOURCE_BRANCH"'",
+        "draft": true
+    }'
+    curl_out="$( curl \
+        -L \
+        --silent \
+        -X POST \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer $GITHUB_TOKEN" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        "https://api.github.com/repos/redhat-performance/backstage-performance/pulls" \
+        -d "$curl_data"
+    )"
+    pr_number=$( echo "$curl_out" | jq -rc '.number' )
+    curl_comment_out="$( curl \
+        -L \
+        --silent \
+        -X POST \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer $GITHUB_TOKEN" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        "https://api.github.com/repos/redhat-performance/backstage-performance/issues/$pr_number/comments" \
+        -d '{"body":"/test mvp-scalability"}'
+    )"
+    comment_url=$( echo "$curl_comment_out" | jq -rc '.html_url' )
+    echo "$(date -Ins --utc) INFO Triggered build by ${comment_url}"
 }
 
 function _test() {
@@ -47,17 +83,32 @@ function _test() {
 
     branch_old="test-$VERSION_NEW-$nick-$VERSION_OLD"
     export RHDH_HELM_REPO="$RHDH_HELM_REPO_OLD"
+    export SOURCE_BRANCH="$SOURCE_BRANCH_OLD"
     configure_run "$ticket" "$branch_old" "$name"
 
     branch_new="test-$VERSION_NEW-$nick-$VERSION_NEW"
     export RHDH_HELM_REPO="$RHDH_HELM_REPO_NEW"
+    export SOURCE_BRANCH="$SOURCE_BRANCH_NEW"
     configure_run "$ticket" "$branch_new" "$name"
+}
+
+function compare_previous_test() {
+    name="Compare to previous release"
+    nick="compare"
+    ticket="$1"   # Jira task for comparison tests
+
+    export DURATION="15m"
+    export SCALE_BS_USERS_GROUPS="1000:250"
+    export SCALE_CATALOG_SIZES="2500:2500"
+    export SCALE_DB_STORAGES="1Gi"
+
+    _test "$name" "$nick" "$ticket"
 }
 
 function entity_burden_test() {
     name="Entity burden test"
     nick="entity"
-    ticket="$1"   # Jira task for storage tests
+    ticket="$1"   # Jira task for entity burden tests
 
     export DURATION="15m"
     export SCALE_BS_USERS_GROUPS="100:20"
@@ -73,17 +124,20 @@ function storage_limit_test() {
     ticket="$1"   # Jira task for storage tests
 
     export DURATION="15m"
-    export SCALE_BS_USERS_GROUPS="10000:2000"
-    export SCALE_CATALOG_SIZES="1:1 2800:2800 2900:2900 3000:3000 3100:3100 3200:3200 3300:3300 3400:3400"
+    export SCALE_BS_USERS_GROUPS="100:20"
+    export SCALE_CATALOG_SIZES="1:1 3000:3000 4000:4000 5000:5000 6000:6000 7000:7000 8000:8000 9000:9000 10000:10000"
     export SCALE_DB_STORAGES="1Gi"
 
     _test "$name" "$nick" "$ticket"
 }
 
 # !!! Configure here !!!
-VERSION_OLD="1.3"
-VERSION_NEW="1.4"
+VERSION_OLD="1.4"
+VERSION_NEW="1.5"
 RHDH_HELM_REPO_OLD="https://charts.openshift.io/"
-RHDH_HELM_REPO_NEW="https://raw.githubusercontent.com/rhdh-bot/openshift-helm-charts/refs/heads/redhat-developer-hub-1.4-117-CI/installation"
-entity_burden_test "RHIDP-4541"
-#storage_limit_test "RHIDP-4531"
+RHDH_HELM_REPO_NEW="https://raw.githubusercontent.com/rhdh-bot/openshift-helm-charts/refs/heads/redhat-developer-hub-1.5-147-CI/installation"
+SOURCE_BRANCH_OLD=rhdh-v1.4.x
+SOURCE_BRANCH_NEW=main
+compare_previous_test "RHIDP-5931"
+entity_burden_test "RHIDP-5929"
+storage_limit_test "RHIDP-5927"
