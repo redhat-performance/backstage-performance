3886ed187b4bc7173896f4fd3395ea3f3ba5d42c
Pavel Macík <pavel.macik@gmail.com>
2025-11-26 20:31:05 +0100

    feat(RHIDP-9874): Add additional jobs, enable running individual scenarios, add label to PR (#268)
    Signed-off-by: Pavel Macík <pavel.macik@gmail.com>

---
+++ ci-scripts/gather-artifacts-from-ci.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+ARTIFACT_DIR=$(python3 -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' "${ARTIFACT_DIR:-.artifacts}")
+mkdir -p "${ARTIFACT_DIR}"
+
+prs="279 280 281 282 283 284 285 286 287 288 289 290"
+branches="rhdh-v1.7.x main"
+
+CURRENT_VERSION=${CURRENT_VERSION:-1.8-164}
+PREVIOUS_VERSION=${PREVIOUS_VERSION:-1.7.2}
+CURRENT_BASE_VERSION=1.8
+PREVIOUS_BASE_VERSION=1.7
+
+gather_artifacts_from_ci() {
+    for PR_NUMBER in ${prs}; do
+        for BRANCH in ${branches}; do
+
+            echo
+            echo "Trying to gather artifacts from PR ${PR_NUMBER} on branch ${BRANCH}..."
+
+            ORG="redhat-performance"
+            REPO="backstage-performance"
+            TEST_NAME="mvp-scalability"
+
+            JOB_NAME="pull-ci-${ORG}-${REPO}-${BRANCH}-${TEST_NAME}"
+
+            BASE_URL="https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/test-platform-results/pr-logs/pull/${ORG}_${REPO}/${PR_NUMBER}/${JOB_NAME}/"
+            LATEST_BUILD=$(curl -sSL "${BASE_URL}/latest-build.txt" |
+                grep -oE '[0-9]{19}' |
+                sort -un |
+                tail -1) || continue
+
+            echo "Latest build ID: $LATEST_BUILD"
+
+            BASE_GS_ARTIFACTS="test-platform-results/pr-logs/pull/${ORG}_${REPO}/${PR_NUMBER}/${JOB_NAME}/${LATEST_BUILD}/artifacts/${TEST_NAME}/${ORG}-${REPO}-scalability/artifacts"
+            RHDH_VERSION=$(gcloud storage cat "gs://${BASE_GS_ARTIFACTS}/scalability/*/test/1/*/benchmark.json" | gunzip | jq -rc '.metadata.env.RHDH_HELM_CHART_VERSION') || continue
+            echo "RHDH version: $RHDH_VERSION"
+
+            SCALABILITY_SCENARIO=$(curl -sSL "https://api.github.com/repos/${ORG}/${REPO}/pulls/${PR_NUMBER}" | jq -rc '.head.ref')
+            echo "Scalability scenario: $SCALABILITY_SCENARIO"
+
+            output="${ARTIFACT_DIR}/.artifacts.${SCALABILITY_SCENARIO}"
+
+            mkdir -p "${output}"
+            echo "$RHDH_VERSION" >"$output/rhdh-version.txt"
+            echo "$PR_NUMBER" >"$output/pr-number.txt"
+            echo "$LATEST_BUILD" >"$output/latest-build.txt"
+
+            gcloud storage cp -r "gs://test-platform-results/pr-logs/pull/${ORG}_${REPO}/${PR_NUMBER}/${JOB_NAME}/${LATEST_BUILD}/artifacts/${TEST_NAME}/${ORG}-${REPO}-scalability/artifacts/summary.csv" "$output" || continue
+            echo "Artifacts gathered from PR ${PR_NUMBER} on branch ${BRANCH}"
+        done
+    done
+}
+
+generate_rhdh_perf_charts() {
+
+    echo "$(date -u -Ins) Generating RHDH performacne charts"
+
+    PREVIOUS_DIR=${PREVIOUS_DIR:-}
+    CURRENT_DIR=${CURRENT_DIR:-}
+    SCENARIO=${SCENARIO:-}
+
+    OUTPUT_DIR=${OUTPUT_DIR:-.backstage-perf-charts}
+
+    metrics="RPS_Avg \
+RPS_Max \
+Response_Time_Avg \
+Response_Time_Max \
+Failures \
+Fail_Ratio_Avg \
+RHDH_CPU_Avg \
+RHDH_CPU_Max \
+RHDH_Memory_Avg \
+RHDH_Memory_Max \
+RHDH_DB_CPU_Avg \
+RHDH_DB_CPU_Max \
+RHDH_DB_Memory_Avg \
+RHDH_DB_Memory_Max \
+Components_Response_Time_Avg \
+Components_Response_Time_Max \
+ComponentsOwnedByUserGroup_Response_Time_Avg \
+ComponentsOwnedByUserGroup_Response_Time_Max \
+RHDH_DB_Populate_Storage_Used \
+RHDH_DB_Test_Storage_Used \
+Orchestrator_Workflow_Overview_Response_Time_Avg \
+Orchestrator_Workflow_Overview_Response_Time_Max \
+Orchestrator_Workflow_Execute_Response_Time_Avg \
+Orchestrator_Workflow_Execute_Response_Time_Max \
+Orchestrator_Workflow_Instance_by_Id_Response_Time_Avg \
+Orchestrator_Workflow_Instance_by_Id_Response_Time_Max \
+Orchestrator_Workflow_All_Instances_Response_Time_Avg \
+Orchestrator_Workflow_All_Instances_Response_Time_Max \
+DeployDuration \
+PopulateDuration \
+PopulateUsersGroupsDuration \
+PopulateCatalogDuration \
+Duration"
+
+    for x_axis_scale_label in "ActiveUsers:linear:Active Users" "RBAC_POLICY_SIZE:log:RBAC Policy Size" "Iteration:linear:Iteration"; do
+        IFS=":" read -ra tokens <<<"${x_axis_scale_label}"
+        xa="${tokens[0]}"                                         # x_axis
+        [[ "${#tokens[@]}" -lt 2 ]] && sc="" || sc="${tokens[1]}" # scale
+        [[ "${#tokens[@]}" -lt 2 ]] && xn="" || xn="${tokens[2]}" # x_label
+        if [ -n "$SCENARIO" ]; then
+            xn="$xn ($SCENARIO)"
+        fi
+        #shellcheck disable=SC2086
+        if [ -n "$PREVIOUS_DIR" ]; then
+            python3 "$SCRIPT_DIR/scalability/rhdh-perf-chart.py" \
+                --previous "$PREVIOUS_DIR/summary.csv" \
+                --previous-version "$PREVIOUS_VERSION" \
+                --current "$CURRENT_DIR/summary.csv" \
+                --current-version "$CURRENT_VERSION" \
+                --metrics $metrics \
+                --metrics-metadata "./ci-scripts/scalability/rhdh-perf-chart_metric-metadata.yaml" \
+                --x-axis "$xa" --x-scale "$sc" --x-label "$xn" --y-scale "$sc" --scenario "$xn" --output-dir "$OUTPUT_DIR"
+        else
+            python3 "$SCRIPT_DIR/scalability/rhdh-perf-chart.py" \
+                --previous "$PREVIOUS_DIR/summary.csv" \
+                --previous-version "$PREVIOUS_VERSION" \
+                --current "$CURRENT_DIR/summary.csv" \
+                --current-version "$CURRENT_VERSION" \
+                --metrics $metrics \
+                --metrics-metadata "./ci-scripts/scalability/rhdh-perf-chart_metric-metadata.yaml" \
+                --x-axis "$xa" --x-scale "$sc" --x-label "$xn" --y-scale "$sc" --scenario "$xn" --output-dir "$OUTPUT_DIR"
+        fi
+    done
+}
+
+generate_rhdh_perf_charts_for_scenarios() {
+    # Comparing current version with previous version for each scenario
+    for s in max_concurrency max_concurrency_with_orchestrator rbac rbac_groups rbac_nested orchestrator; do
+
+        export CURRENT_DIR="${ARTIFACT_DIR}/.artifacts.test-${CURRENT_BASE_VERSION}-${s}-${CURRENT_BASE_VERSION}"
+        export PREVIOUS_DIR="${ARTIFACT_DIR}/.artifacts.test-${CURRENT_BASE_VERSION}-${s}-${PREVIOUS_BASE_VERSION}"
+
+        export SCENARIO="$s"
+        export OUTPUT_DIR="${ARTIFACT_DIR}/.backstage-perf-charts/$s"
+
+        export CURRENT_VERSION PREVIOUS_VERSION
+        CURRENT_VERSION=$(cat "${CURRENT_DIR}/rhdh-version.txt") || continue
+        PREVIOUS_VERSION=$(cat "${PREVIOUS_DIR}/rhdh-version.txt") || continue
+
+        generate_rhdh_perf_charts
+    done
+
+    # Comparing orchestrator overhead
+    export CURRENT_DIR="${ARTIFACT_DIR}/.artifacts.test-${CURRENT_BASE_VERSION}-max_concurrency_with_orchestrator-${CURRENT_BASE_VERSION}"
+    export PREVIOUS_DIR="${ARTIFACT_DIR}/.artifacts.test-${CURRENT_BASE_VERSION}-max_concurrency-${CURRENT_BASE_VERSION}"
+
+    export SCENARIO=max_concurrency_with_orchestrator_overhead
+    export OUTPUT_DIR="${ARTIFACT_DIR}/.backstage-perf-charts/${SCENARIO}"
+    export CURRENT_VERSION PREVIOUS_VERSION
+    CURRENT_VERSION="Orchestrator<br>($(cat "${CURRENT_DIR}/rhdh-version.txt"))" || return 1
+    PREVIOUS_VERSION="No orchestrator<br>($(cat "${PREVIOUS_DIR}/rhdh-version.txt"))" || return 1
+    generate_rhdh_perf_charts
+}
+
+all() {
+    gather_artifacts_from_ci
+    generate_rhdh_perf_charts_for_scenarios
+}
+
+help() {
+    echo "Usage: $0 [-a] [-g] [-c] [-h]"
+    echo "  -a: Gather artifacts from CI and generate RHDH performance charts for all scenarios"
+    echo "  -g: Gather artifacts from CI only"
+    echo "  -c: Generate RHDH performance charts for all scenarios only"
+    echo "  -h: Show this help message"
+}
+
+if [ $# -eq 0 ]; then
+    help
+fi
+
+while getopts "agch" flag; do
+    case "${flag}" in
+    g)
+        gather_artifacts_from_ci
+        ;;
+    c)
+        generate_rhdh_perf_charts_for_scenarios
+        ;;
+    a)
+        all
+        ;;
+    \? | h)
+        help
+        ;;
+    esac
+done
--- ci-scripts/release-tests.sh
+++ ci-scripts/release-tests.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-set -eu
+set -u
+
+TMP_DIR=$(python3 -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' "${TMP_DIR:-.tmp}")
+mkdir -p "${TMP_DIR}"
+
+WS="${TMP_DIR}/backstage-performance.git"
+
+rm -rf "$WS"
+git clone git@github.com:redhat-performance/backstage-performance.git "${WS}"
+cd "${WS}" || exit 1
 
 function configure_run() {
     if ! [ -f test.env ]; then
@@ -23,52 +32,51 @@ function configure_run() {
         echo "$(date -u +'%Y-%m-%dT%H:%M:%S,%N+00:00') INFO Branch $branch exists, updating it"
         git checkout "$branch" 2>/dev/null || git checkout -b "$branch" origin/"$branch"
         git pull origin "$branch"
+        git rebase origin/"$SOURCE_BRANCH"
     else
         echo "$(date -u +'%Y-%m-%dT%H:%M:%S,%N+00:00') INFO Creating new branch $branch"
         git checkout -b "$branch"
     fi
 
-    if [ -f test.env ]; then
-        sed -i.bak '/^export DURATION=/,/^export RHDH_HELM_CHART_VERSION=/d' test.env
-        rm -f test.env.bak
-    fi
-
     echo "
-export DURATION=$DURATION
-export PRE_LOAD_DB=true
-export SCALE_ACTIVE_USERS_SPAWN_RATES='100:5'
-export SCALE_BS_USERS_GROUPS='$SCALE_BS_USERS_GROUPS'
-export SCALE_CATALOG_SIZES='$SCALE_CATALOG_SIZES'
-export SCALE_CPU_REQUESTS_LIMITS=:
-export SCALE_DB_STORAGES='$SCALE_DB_STORAGES'
-export SCALE_MEMORY_REQUESTS_LIMITS=:
-export SCALE_REPLICAS=1:1
-export SCALE_WORKERS=20
-export ENABLE_RBAC=true
-export SCENARIO=mvp
-export USE_PR_BRANCH=true
-export WAIT_FOR_SEARCH_INDEX=false
-export RHDH_HELM_CHART=redhat-developer-hub
-export AUTH_PROVIDER=keycloak
-export ENABLE_ORCHESTRATOR=true
-export RHDH_HELM_CHART_VERSION='$RHDH_HELM_CHART_VERSION'
-" >>test.env
-
+export DURATION=${DURATION:-}
+export PRE_LOAD_DB=${PRE_LOAD_DB:-true}
+export SCALE_ACTIVE_USERS_SPAWN_RATES='${SCALE_ACTIVE_USERS_SPAWN_RATES:-100:5}'
+export SCALE_BS_USERS_GROUPS='${SCALE_BS_USERS_GROUPS:-}'
+export SCALE_CATALOG_SIZES='${SCALE_CATALOG_SIZES:-}'
+export SCALE_CPU_REQUESTS_LIMITS='${SCALE_CPU_REQUESTS_LIMITS:-:}'
+export SCALE_DB_STORAGES='${SCALE_DB_STORAGES:-}'
+export SCALE_MEMORY_REQUESTS_LIMITS='${SCALE_MEMORY_REQUESTS_LIMITS:-:}'
+export SCALE_REPLICAS='${SCALE_REPLICAS:-1:1}'
+export SCALE_WORKERS='${SCALE_WORKERS:-20}'
+export SCALE_RBAC_POLICY_SIZE='${SCALE_RBAC_POLICY_SIZE:-1000}'
+export RBAC_POLICY='${RBAC_POLICY:-all_groups_admin}'
+export ENABLE_RBAC=${ENABLE_RBAC:-true}
+export SCENARIO=${SCENARIO:-mvp}
+export USE_PR_BRANCH=${USE_PR_BRANCH:-true}
+export WAIT_FOR_SEARCH_INDEX=${WAIT_FOR_SEARCH_INDEX:-false}
+export RHDH_HELM_CHART=${RHDH_HELM_CHART:-redhat-developer-hub}
+export AUTH_PROVIDER=${AUTH_PROVIDER:-keycloak}
+export ENABLE_ORCHESTRATOR=${ENABLE_ORCHESTRATOR:-true}
+export RHDH_HELM_CHART_VERSION=${RHDH_HELM_CHART_VERSION:-}
+export ALWAYS_CLEANUP=${ALWAYS_CLEANUP:-false}
+" >test.env
     git commit -am "chore($ticket): $testname on $branch"
-    git push -u origin "$branch"
+    git push -fu origin "$branch"
     echo "$(date -u +'%Y-%m-%dT%H:%M:%S,%N+00:00') INFO Pushed branch ${branch}"
     git checkout "$SOURCE_BRANCH"
 
-
-    pr_number=$( curl -L --silent \
-        -H "Accept: application/vnd.github+json" \
-        -H "Authorization: Bearer $GITHUB_TOKEN" \
-        -H "X-GitHub-Api-Version: 2022-11-28" \
-        "https://api.github.com/repos/redhat-performance/backstage-performance/pulls?head=redhat-performance:$branch&state=open" \
-        | jq -rc '.[0].number // empty'
+    sleep 5s
+    pr_number=$(
+        curl -L --silent \
+            -H "Accept: application/vnd.github+json" \
+            -H "Authorization: Bearer $GITHUB_TOKEN" \
+            -H "X-GitHub-Api-Version: 2022-11-28" \
+            "https://api.github.com/repos/redhat-performance/backstage-performance/pulls?head=redhat-performance:$branch&state=open" |
+            jq -rc '.[0].number // empty'
     )
 
-    if [ -z "$pr_number" ] ; then
+    if [ -z "$pr_number" ]; then
         curl_data='{
             "title": "chore('"$ticket"'): '"$branch"'",
             "body": "**'"$testname"'**: '"$VERSION_OLD"' vs. '"$VERSION_NEW"' testing. This is to get perf&scale data for `'"$branch"'`",
@@ -76,31 +84,45 @@ export RHDH_HELM_CHART_VERSION='$RHDH_HELM_CHART_VERSION'
             "base": "'"$SOURCE_BRANCH"'",
             "draft": true
         }'
-        curl_out="$( curl \
+        curl_out="$(
+            curl \
+                -L \
+                --silent \
+                -X POST \
+                -H "Accept: application/vnd.github+json" \
+                -H "Authorization: Bearer $GITHUB_TOKEN" \
+                -H "X-GitHub-Api-Version: 2022-11-28" \
+                "https://api.github.com/repos/redhat-performance/backstage-performance/pulls" \
+                -d "$curl_data"
+        )"
+        pr_number=$(echo "$curl_out" | jq -rc '.number')
+    fi
+
+    curl_comment_out="$(
+        curl \
             -L \
             --silent \
             -X POST \
             -H "Accept: application/vnd.github+json" \
             -H "Authorization: Bearer $GITHUB_TOKEN" \
             -H "X-GitHub-Api-Version: 2022-11-28" \
-            "https://api.github.com/repos/redhat-performance/backstage-performance/pulls" \
-            -d "$curl_data"
-        )"
-        pr_number=$( echo "$curl_out" | jq -rc '.number' )
-    fi
+            "https://api.github.com/repos/redhat-performance/backstage-performance/issues/$pr_number/comments" \
+            -d '{"body":"/test mvp-scalability"}'
+    )"
+    comment_url=$(echo "$curl_comment_out" | jq -rc '.html_url')
+    echo "$(date -u +'%Y-%m-%dT%H:%M:%S,%N+00:00') INFO Triggered build by ${comment_url}"
 
-    curl_comment_out="$( curl \
+    curl \
         -L \
         --silent \
         -X POST \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer $GITHUB_TOKEN" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        "https://api.github.com/repos/redhat-performance/backstage-performance/issues/$pr_number/comments" \
-        -d '{"body":"/test mvp-scalability"}'
-    )"
-    comment_url=$( echo "$curl_comment_out" | jq -rc '.html_url' )
-    echo "$(date -u +'%Y-%m-%dT%H:%M:%S,%N+00:00') INFO Triggered build by ${comment_url}"
+        "https://api.github.com/repos/redhat-performance/backstage-performance/issues/$pr_number/labels" \
+        -d '{"labels":["release-tests/in-progress"]}'
+
+    echo "$(date -u +'%Y-%m-%dT%H:%M:%S,%N+00:00') INFO Added label 'release-tests/in-progress' to PR #${pr_number}"
 }
 
 function _test() {
@@ -122,7 +144,7 @@ function _test() {
 function compare_previous_test() {
     name="Compare to previous release"
     nick="compare"
-    ticket="$1"   # Jira task for comparison tests
+    ticket="$1" # Jira task for comparison tests
 
     export DURATION="15m"
     export SCALE_BS_USERS_GROUPS="1000:250 1000:250 1000:250 1000:250 1000:250"
@@ -135,7 +157,7 @@ function compare_previous_test() {
 function entity_burden_test() {
     name="Entity burden test"
     nick="entity"
-    ticket="$1"   # Jira task for entity burden tests
+    ticket="$1" # Jira task for entity burden tests
 
     export DURATION="15m"
     export SCALE_BS_USERS_GROUPS="100:20"
@@ -148,12 +170,146 @@ function entity_burden_test() {
 function storage_limit_test() {
     name="Storage limit test"
     nick="storage"
-    ticket="$1"   # Jira task for storage tests
+    ticket="$1" # Jira task for storage tests
 
     export DURATION="15m"
     export SCALE_BS_USERS_GROUPS="100:20"
     export SCALE_CATALOG_SIZES="1:1 3000:3000 4000:4000 5000:5000 6000:6000 7000:7000 8000:8000 9000:9000 10000:10000"
     export SCALE_DB_STORAGES="1Gi"
+
+    _test "$name" "$nick" "$ticket"
+}
+
+function max_concurrency_test() {
+    name="Max Concurrency test"
+    nick="max_concurrency"
+    ticket="$1" # Jira task for storage tests
+
+    export DURATION="10m"
+    export RHDH_LOG_LEVEL=debug
+    export SCALE_WORKERS=100
+    export RBAC_POLICY=all_groups_admin
+    export ENABLE_ORCHESTRATOR=false
+
+    export SCALE_ACTIVE_USERS_SPAWN_RATES="1:1 10:2 25:5 50:10 100:20 150:30 200:40 250:50 300:60 350:70 400:80 500:100"
+    export SCALE_BS_USERS_GROUPS="1000:250"
+    export SCALE_RBAC_POLICY_SIZE="1000"
+    export SCALE_CATALOG_SIZES="2500:2500"
+    export SCALE_REPLICAS="1:1"
+    export SCALE_DB_STORAGES="2Gi"
+    export SCALE_CPU_REQUESTS_LIMITS=":"
+    export SCALE_MEMORY_REQUESTS_LIMITS=":"
+
+    _test "$name" "$nick" "$ticket"
+}
+
+function max_concurrency_with_orchestrator_test() {
+    name="Max Concurrency with Orchestrator test"
+    nick="max_concurrency_with_orchestrator"
+    ticket="$1" # Jira task for storage tests
+
+    export DURATION="10m"
+    export RHDH_LOG_LEVEL=debug
+    export SCALE_WORKERS=100
+    export RBAC_POLICY=all_groups_admin
+    export ENABLE_ORCHESTRATOR=true
+
+    export SCALE_ACTIVE_USERS_SPAWN_RATES="1:1 10:2 25:5 50:10 100:20 150:30 200:40 250:50 300:60 350:70 400:80 500:100"
+    export SCALE_BS_USERS_GROUPS="1000:250"
+    export SCALE_RBAC_POLICY_SIZE="1000"
+    export SCALE_CATALOG_SIZES="2500:2500"
+    export SCALE_REPLICAS="1:1"
+    export SCALE_DB_STORAGES="2Gi"
+    export SCALE_CPU_REQUESTS_LIMITS=":"
+    export SCALE_MEMORY_REQUESTS_LIMITS=":"
+
+    _test "$name" "$nick" "$ticket"
+}
+
+function orchestrator_test() {
+    name="Orchestrator test"
+    nick="orchestrator"
+    ticket="$1" # Jira task for storage tests
+
+    export DURATION="10m"
+    export RHDH_LOG_LEVEL=debug
+    export SCALE_WORKERS=100
+    export RBAC_POLICY=all_groups_admin
+    export ENABLE_ORCHESTRATOR=true
+    export SCENARIO=orchestrator
+    export ALWAYS_CLEANUP=true
+
+    export SCALE_ACTIVE_USERS_SPAWN_RATES="1:1 10:2 25:5 50:10 100:20 150:30 200:40 250:50 300:60 400:80 500:100"
+    export SCALE_BS_USERS_GROUPS="1000:250"
+    export SCALE_RBAC_POLICY_SIZE="1000"
+    export SCALE_CATALOG_SIZES="2500:2500"
+    export SCALE_REPLICAS="1:1"
+    export SCALE_DB_STORAGES="2Gi"
+    export SCALE_CPU_REQUESTS_LIMITS=":"
+    export SCALE_MEMORY_REQUESTS_LIMITS=":"
+
+    _test "$name" "$nick" "$ticket"
+}
+
+function rbac_test() {
+    name="RBAC test"
+    nick="rbac"
+    ticket="$1" # Jira task for storage tests
+
+    export DURATION="10m"
+    export RBAC_POLICY=all_groups_admin
+    export ENABLE_ORCHESTRATOR=false
+
+    export SCALE_ACTIVE_USERS_SPAWN_RATES="1:1"
+    export SCALE_BS_USERS_GROUPS="1000:250"
+    export SCALE_RBAC_POLICY_SIZE="1 10 100 1000 2000 4000 8000 10000 15000 20000 25000 26000"
+    export SCALE_CATALOG_SIZES="2500:2500"
+    export SCALE_REPLICAS="1:1"
+    export SCALE_DB_STORAGES="2Gi"
+    export SCALE_CPU_REQUESTS_LIMITS=":"
+    export SCALE_MEMORY_REQUESTS_LIMITS=":"
+
+    _test "$name" "$nick" "$ticket"
+}
+
+function rbac_groups_test() {
+    name="RBAC Groups test"
+    nick="rbac_groups"
+    ticket="$1" # Jira task for storage tests
+
+    export DURATION="10m"
+    export RBAC_POLICY=user_in_multiple_groups
+    export ENABLE_ORCHESTRATOR=false
+
+    export SCALE_ACTIVE_USERS_SPAWN_RATES="1:1"
+    export SCALE_BS_USERS_GROUPS="1000:1000"
+    export SCALE_RBAC_POLICY_SIZE="1 10 50 100 200 300 400 500 750 1000"
+    export SCALE_CATALOG_SIZES="2500:2500"
+    export SCALE_REPLICAS="1:1"
+    export SCALE_DB_STORAGES="2Gi"
+    export SCALE_CPU_REQUESTS_LIMITS=":"
+    export SCALE_MEMORY_REQUESTS_LIMITS=":"
+
+    _test "$name" "$nick" "$ticket"
+}
+
+function rbac_nested_test() {
+    name="RBAC Nested test"
+    nick="rbac_nested"
+    ticket="$1" # Jira task for storage tests
+
+    export DURATION="10m"
+    export RBAC_POLICY=nested_groups
+    export ENABLE_ORCHESTRATOR=false
+
+    export SCALE_ACTIVE_USERS_SPAWN_RATES="1:1"
+    export SCALE_BS_USERS_GROUPS="1000:1000"
+    export SCALE_RBAC_POLICY_SIZE="1 10 50 100 200 300 400 500 750 1000"
+    export SCALE_CATALOG_SIZES="2500:2500"
+    export SCALE_REPLICAS="1:1"
+    export SCALE_DB_STORAGES="2Gi"
+    export SCALE_CPU_REQUESTS_LIMITS=":"
+    export SCALE_MEMORY_REQUESTS_LIMITS=":"
 
     _test "$name" "$nick" "$ticket"
 }
@@ -165,6 +321,79 @@ RHDH_HELM_CHART_VERSION_OLD=1.7.2
 RHDH_HELM_CHART_VERSION_NEW=1.8-164-CI
 SOURCE_BRANCH_OLD=rhdh-v1.7.x
 SOURCE_BRANCH_NEW=main
-compare_previous_test "RHIDP-9162"
-entity_burden_test "RHIDP-9167"
-storage_limit_test "RHIDP-9163"
+
+run_compare_previous_test() {
+    compare_previous_test "RHIDP-9162"
+}
+run_entity_burden_test() {
+    entity_burden_test "RHIDP-9167"
+}
+run_storage_limit_test() {
+    storage_limit_test "RHIDP-9163"
+}
+run_max_concurrency_test() {
+    max_concurrency_test "RHIDP-9158"
+}
+run_max_concurrency_with_orchestrator_test() {
+    max_concurrency_with_orchestrator_test "RHIDP-9159"
+}
+run_orchestrator_test() {
+    orchestrator_test "RHIDP-9708"
+}
+run_rbac_test() {
+    rbac_test "RHIDP-9165"
+}
+run_rbac_groups_test() {
+    rbac_groups_test "RHIDP-9171"
+}
+run_rbac_nested_test() {
+    rbac_nested_test "RHIDP-9173"
+}
+
+IFS="," read -ra test_ids <<<"${1:-all}"
+for test_id in "${test_ids[@]}"; do
+    case $test_id in
+    "compare_previous")
+        run_compare_previous_test
+        ;;
+    "entity_burden")
+        run_entity_burden_test
+        ;;
+    "storage_limit")
+        run_storage_limit_test
+        ;;
+    "max_concurrency")
+        run_max_concurrency_test
+        ;;
+    "max_concurrency_with_orchestrator")
+        run_max_concurrency_with_orchestrator_test
+        ;;
+    "orchestrator")
+        run_orchestrator_test
+        ;;
+    "rbac")
+        run_rbac_test
+        ;;
+    "rbac_groups")
+        run_rbac_groups_test
+        ;;
+    "rbac_nested")
+        run_rbac_nested_test
+        ;;
+    \? | "all")
+        run_compare_previous_test
+        run_entity_burden_test
+        run_storage_limit_test
+        # run_max_concurrency_test
+        # run_max_concurrency_with_orchestrator_test
+        # run_orchestrator_test
+        # run_rbac_test
+        # run_rbac_groups_test
+        # run_rbac_nested_test
+        ;;
+    *)
+        echo "$(date -u +'%Y-%m-%dT%H:%M:%S,%N+00:00') ERROR Unsupported test option: '$test_id'"
+        exit 1
+        ;;
+    esac
+done
